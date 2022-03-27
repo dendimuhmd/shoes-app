98bac08d8d90188a456c74f3345ce12a631792ff
dendimuhmd <muhammaddendi27@gmail.com>
2022-03-27 09:07:56 +0700

    find method

--- lib/page/home_screen.dart
+++ lib/page/home_screen.dart
@@ -17,7 +17,11 @@ class _HomePageState extends State<HomePage> {
   void addShoes() {
     final shoes = Shoes(
         name: inputShoesController.text,
-        price: int.parse(inputPriceController.text),
+        price: (inputPriceController.text.isEmpty ||
+                inputPriceController.text !=
+                    int.parse(inputPriceController.text))
+            ? null
+            : int.parse(inputPriceController.text),
         date: DateTime.now());
     if (shoes == null) return;
     setState(() {
@@ -28,66 +32,69 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Card(
-            elevation: 10,
-            child: Container(
-              width: double.infinity,
-              height: 400,
-              child: Column(
-                children: shoesList
-                    .map((e) => Card(
-                          child: Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                  color: Colors.black,
-                                  width: 2,
-                                )),
-                                margin: EdgeInsets.all(10),
-                                child: Text(e.price.toStringAsFixed(2)),
-                              ),
-                              Container(
-                                child: Text(e.name),
-                              )
-                            ],
-                          ),
-                        ))
-                    .toList(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Card(
+              elevation: 10,
+              child: Container(
+                width: double.infinity,
+                height: 400,
+                child: Column(
+                  children: shoesList
+                      .map((e) => Card(
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  )),
+                                  margin: EdgeInsets.all(10),
+                                  child: Text(
+                                      "${(e.price == null) ? null : e.price!.toStringAsFixed(2)}"),
+                                ),
+                                Container(
+                                  child: Text(e.name),
+                                )
+                              ],
+                            ),
+                          ))
+                      .toList(),
+                ),
               ),
             ),
-          ),
-          Card(
-            elevation: 20,
-            child: Container(
-              margin: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: 'input shoes...'),
-                    controller: inputShoesController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'price...'),
-                    controller: inputPriceController,
-                  ),
-                ],
+            Card(
+              elevation: 20,
+              child: Container(
+                margin: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'input shoes...'),
+                      controller: inputShoesController,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'price...'),
+                      controller: inputPriceController,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(onPressed: addShoes, icon: Icon(IconlyBold.bag)),
-              IconButton(onPressed: () {}, icon: Icon(IconlyBold.delete)),
-            ],
-          )
-        ],
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(onPressed: addShoes, icon: Icon(IconlyBold.bag)),
+                IconButton(onPressed: () {}, icon: Icon(IconlyBold.delete)),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
